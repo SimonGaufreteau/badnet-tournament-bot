c6435d7e8d338fe4ec247b437d08fc03694dc220
SimonGaufreteau <simon.gaufreteau@gmail.com>
2025-09-11 16:50:02 +0200

    Implement category filter

--- src/filters.ts
+++ src/filters.ts
@@ -9,6 +9,7 @@ export const filterTournaments = (
     .filter(filterPastTournaments)
     .filter((t) => filterOpenTournaments(t, filters))
     .filter((t) => filterClosedTournaments(t, filters))
+    .filter((t) => filterAgeCategories(t, filters))
 
 const filterPastTournaments = (t: Tournament): boolean =>
   dayjs.unix(t.lastDay).isAfter(dayjs())
@@ -24,3 +25,9 @@ const filterClosedTournaments = (
   { hideClosedTournaments }: Filters,
 ): boolean =>
   hideClosedTournaments ? dayjs.unix(t.truedeadline).isAfter(dayjs()) : true
+
+const filterAgeCategories = (t: Tournament, { ageCategories }: Filters) => {
+  const validCategories = new Set(ageCategories)
+  if (ageCategories.length == 0) return true
+  return t.ageCategories.filter((cat) => validCategories.has(cat)).length > 0
+}
